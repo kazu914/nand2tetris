ee94c434c4ef77eb57809b3625aca07f5c1eea89
Kazuaki Nomura <nkz0914ssk@gmail.com>
2020-10-16 11:26:24 +0900

    implemtentd FILL.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,42 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// 16384~24575 screen
+// 24576  keyboard
+(LOOP)
+	@24576
+	D=M
+	@ON
+	D;JGT
+	@OFF
+	D;JMP
+(ON)
+	@value
+	M=-1
+	@FILL
+	0;JMP
+(OFF)
+	@value
+	M=0
+	@FILL
+	0;JMP
+(FILL)
+	@16384
+	D=A
+	@loc
+	M=D
+	(REPEAT)
+		@value
+		D=M
+		@loc
+		A=M
+		M=D
+		@loc
+		D=M+1
+		M=D
+		@24575
+		D=D-A
+		@REPEAT
+		D;JLE
+	@LOOP
+	0;JMP
